7f8051d62ec8458af8088f2f977f2e607d2bcb9a
Programmr <michal.pelon@proton.me>
2025-11-07 22:56:03 +0100

    LAB4B beta 1.1 - chmod added

--- LAB5/runall.sh
+++ LAB5/runall.sh
@@ -5,6 +5,13 @@ gcc ./dec2bin.c -o ./dec2bin
 gcc ./decfrac2bin.c -o ./decfrac2bin
 echo "Compiled!"
 
+
+chmod +x ./bin2dec
+chmod +x ./dec2bin
+chmod +x ./decfrac2bin
+chmod +x ./gen_numbers.sh
+chmod +x ./test_rounttrip.sh
+
 echo "Generating numbers..."
 ./gen_numbers.sh
 echo "Number ready!"
