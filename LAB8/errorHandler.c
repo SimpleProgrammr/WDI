1a79ca39250b1c5d4065f8e8029bbedb9f8d24ee
Elmo <michal.pelon@proton.me>
2025-12-12 13:18:15 +0100

    PostErrorMsg() added

---
+++ LAB8/errorHandler.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ERROR(char * msg, int code) {
+    fprintf(stderr, "%s\n", msg);
+    exit(code);
+}
+
+void PostErrorMsg(char * msg) {
+    fprintf(stderr, "%s\n", msg);
+}
